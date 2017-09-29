7f264822d98155e62006c4e720fbf5d9f503db05
Ryan P. Brewster <RyanPBrewster@gmail.com>
2017-09-28 20:00:02 -0700

    Introduce Item and Metric

--- src/algo.rs
+++ src/algo.rs
@@ -1,7 +1,7 @@
-use {Problem, Solution};
+use {Item, Problem, Solution};
 
-pub fn trivial<T: Clone>(problem: &Problem<T>) -> Solution<T> {
+pub fn trivial<T: Item>(problem: &Problem<T>) -> Solution<T> {
     let mut bins = vec![Vec::new(); problem.num_bins];
     bins[0].clone_from(&problem.items);
-    Solution { bins: bins }
+    Solution { bins }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,44 @@
+use std::cmp;
+
+pub trait Item: Clone {}
+
+mod items {
+    use Item;
+    impl Item for i32 {}
+}
+
 #[derive(Debug)]
-pub struct Problem<T: Clone> {
+pub struct Problem<T: Item> {
     pub items: Vec<T>,
     pub num_bins: usize,
 }
 
 #[derive(Debug)]
-pub struct Solution<T: Clone> {
+pub struct Solution<T: Item> {
     pub bins: Vec<Vec<T>>,
+}
+
+pub trait Metric {
+    fn for_bin<T: Item>(bin: &[T]) -> f64;
+
+    fn for_solution<T: Item>(solution: &Solution<T>) -> f64 {
+        solution
+            .bins
+            .iter()
+            .map(|bin| Self::for_bin(bin))
+            .max_by(|x, y| x.partial_cmp(y).unwrap())
+            .expect("Metric::for_solution called on solution with 0 bins")
+    }
+}
+
+pub mod metrics {
+    use Metric;
+    pub struct L0;
+    impl Metric for L0 {
+        fn for_bin<T>(bin: &[T]) -> f64 {
+            0.0
+        }
+    }
 }
 
 pub mod algo;
